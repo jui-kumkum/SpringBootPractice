375d3c104b322c9e53ec5ef6b23b0729d9a93158
Kumkum <kumkumakterjui30@gmail.com>
2023-12-25 19:14:20 -0700

    Creating a Service

--- src/main/java/com/example/Employee/Application/controller/EmployeeController.java
+++ src/main/java/com/example/Employee/Application/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.example.Employee.Application.controller;
 
 import com.example.Employee.Application.entity.Employee;
+import com.example.Employee.Application.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,13 +14,11 @@ import java.util.List;
 @ResponseBody
 public class EmployeeController {
 
+    @Autowired
+    EmployeeService employeeService;
+
     @RequestMapping("/employees")
-    public List<Employee> findAllEmployees(){
-        List<Employee> employeeList = Arrays.asList(
-                new Employee(1, "Kumkum", "Dhaka"),
-                new Employee(2, "Soumi", "Kolkata"),
-                new Employee(3, "Akila", "Tamil")
-        );
-        return employeeList;
+    public List<Employee> findAllEmployees() {
+        return employeeService.getAllEmployees();
     }
 }
---
+++ src/main/java/com/example/Employee/Application/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.example.Employee.Application.service;
+
+import org.springframework.stereotype.Service;
+import com.example.Employee.Application.entity.Employee;
+
+import java.util.Arrays;
+import java.util.List;
+@Service
+public class EmployeeService {
+    List<Employee> employeeList = Arrays.asList(
+            new Employee(1, "Kumkum", "Dhaka"),
+            new Employee(2, "Soumi", "Kolkata"),
+            new Employee(3, "Akila", "Tamil")
+    );
+    public List<Employee> getAllEmployees() {
+        return employeeList;
+    }
+
+}
